0885a666a581295b51aad8c9058e570a6bc5863d
tehHax0r793 <tehhax0r793@gmail.com>
2016-12-25 01:55:54 -0600

    Raw save state view, ala Minerva, #42

---
+++ TiTsEd/View/RawStateViewPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using System.Windows.Controls;
+using TiTsEd.ViewModel;
+
+namespace TiTsEd.View {
+    /// <summary>
+    /// Interaction logic for RawStateViewPage.xaml
+    /// </summary>
+    public partial class RawStateViewPage : UserControl {
+        public RawStateViewPage() {
+            InitializeComponent();
+            DisplayObjectGraph(VM.Instance.CurrentFile);
+        }
+
+        void DisplayObjectGraph(object graph) {
+            var hierarchy = new ObjectViewModelHierarchy(graph);
+            RawSaveStateTree.DataContext = hierarchy;
+        }
+    }
+}
